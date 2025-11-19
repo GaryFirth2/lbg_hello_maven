842e3ef2dba9487c03b7858950465247cd24bf1a
Gary Firth <gary.firth@hotmail.co.uk>
2025-11-19 12:15:06 +0000

    changed to version 3

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -13,6 +13,7 @@ public class App
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
+        System.out.println("this is version3");
         System.out.println(sayGoodbye());
     }
 
